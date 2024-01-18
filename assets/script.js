37a263b8e5cdfea581529910113899edeff1f95d
DavideTarantino <davidetarantino2005@gmail.com>
2024-01-18 13:08:17 +0100

    setup degli elementi

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const dataAttuale = new Date();
+const dataProssima = new Date("2024-01-19 , 09:30");
+
+document.getElementById("data-attuale").innerHTML = `<h3> ${dataAttuale} </h3>`
+document.getElementById("data-prossima").innerHTML = `<h3> ${dataProssima} </h3>`
+
